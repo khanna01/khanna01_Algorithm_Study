594dcbba4783768dc3e1891036146e07e7bce8f0
김한나 <79962121+khanna01@users.noreply.github.com>
2024-08-06 14:47:08 +0900

    [level 0] Title: 암호 해독, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ JavaScript/프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,8 +1,4 @@
 function solution(cipher, code) {
     var answer = '';
-    let cipher_array = cipher.split('')
-    for(let i = code-1; i < cipher_array.length; i= i + code) {
-        answer += cipher_array[i]
-    }
     return answer;
 }
